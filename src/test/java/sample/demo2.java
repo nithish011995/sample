e245ec9557c4dfc184bcb765ac1d973ea323ba5e
admin <admin@DESKTOP-ROIRK3H>
2023-08-16 14:33:14 +0530

    commit and push by AT

--- src/test/java/sample/demo2.java
+++ src/test/java/sample/demo2.java
@@ -7,6 +7,8 @@ public static void main(String[] args) {
 		System.out.println("Hi this is my second commit");
 		
 		System.out.println("life is short");
+		
+		System.out.println("user dead by AT");
 }
 
 }
