6cf46f4030957d14ff0110e66cbcefbdd1fb63eb
admin <admin@DESKTOP-ROIRK3H>
2023-08-16 08:32:16 +0530

    Demo is comitted

---
+++ src/test/java/sample/demo.java
@@ -0,0 +1,13 @@
+package sample;
+
+import org.testng.annotations.Test;
+
+public class demo {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hi this is my first commit");
+	
+	
+}
+}
